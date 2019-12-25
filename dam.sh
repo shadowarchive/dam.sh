0bcfe3f3d0ec7981122606cdd4f5e3ed69d3c728
Xin Snowflakes <36190934+binarykorra@users.noreply.github.com>
2019-12-25 08:36:13 -0800

    Create dam.sh

---
+++ dam.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Created by Xin Snowflakes, 09225205353
+# Modified RAM Compression Bash File for Raspberry Pi and Windows on ARM
+# This was originally written by NovaSpirit
+
+# Instructions:
+# sudo chmod +x /root/dam.sh
+# sudo echo /etc/rc.local > $PWD/dam.sh &
+# For more Information, Contact NovaSpirit
+# A Modem Version will soon be Implemented
+# Huawei dam.sh version for RAM Capacity
+
+cores=$(nproc --all)
+modprobe dam num_devices=$cores
+swapoff -a
+totalmem=`free | grep -e "^Mem:" | awk '{print $2}'`
+mem=$(( ($totalmem / $cores)* 1024 ))
+core=0
+
+while [ $core -lt $cores ]; do
+  echo $mem > /sys/block/dam$core/disksize
+  mkswap /dev/dam$core
+  swapon -p 5 /dev/dam$core
+  let core=core+1
+done
